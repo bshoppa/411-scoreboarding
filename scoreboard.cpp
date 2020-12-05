c612f37971376dd4f4579b609992a52df90af68e
Bailey Shoppa <bshoppa1@umbc.edu>
2020-12-04 21:28:34 -0500

    - added example images - added more advanced scoreboard construction

--- scoreboard.cpp
+++ scoreboard.cpp
@@ -14,8 +14,13 @@ enum InstructionType {
 
 class Subprocessor {
 public:
-	int[4] clockCycles;
+	Subprocessor(clockCycle[4] t_clockCycles, int t_units) {
+		clockCycles = t_clockCycles;
+		units = t_units;
+	}
+	clockCycle[4] clockCycles;
 	int units = 1;
+	string name;
 };
 
 class AInput {
@@ -81,7 +86,7 @@ public:
 			temp.reg2 = buf;
 			temp.reg3 = buf;
 
-			Instructions.psuh_back(temp);
+			Instructions.push_back(temp);
 		}
 	}
 
@@ -118,21 +123,40 @@ public:
 
 };
 
-int main(int argc, char* argv[]) {
-	Scoreboard myInput {
-		.Status = {
-			{"L.D", {.clockCycles = {1, 1, 1, 1}}},
-			{"S.D", {.clockCycles = {1, 1, 1, 1}}},
-			{"ADD", {.clockCycles = {1, 1, 2, 1}}},
-			{"ADDI", {.clockCycles = {1, 1, 10, 1}, .units = 2}},
-			{"", {.clockCycles = {1, 1, 40, 1}}},
-		},
+void load(Scoreboard &scoreboard, string arg1, string arg2);
+void store(Scoreboard &scoreboard, string arg1, string arg2);
+void fp_add(Scoreboard &scoreboard, string arg1, string arg2);
+void fp_multiply(Scoreboard &scoreboard, string arg1, string arg2);
+void fp_divide(Scoreboard &scoreboard, string arg1, string arg2);
 
+int main(int argc, char* argv[]) {
+	Scoreboard myInput (
+		vector<pair<string, Subprocessor>> {
+			pair<string, Subprocessor>("INTEGER UNIT", Subprocessor {{1, 1, 1, 1}, 1}),
+			pair<string, Subprocessor>("FP ADDER", Subprocessor {{1, 1, 2, 1}, 1}),
+			pair<string, Subprocessor>("FP MULTIPLIER", Subprocessor {{1, 1, 10, 1}, 1}),
+			pair<string, Subprocessor>("FP DIVIDER", Subprocessor {{1, 1, 40, 1}, 2})
+		},
+		vector<pair<string, pair<string, void*(Scoreboard, string, string)>>>{
+			pair<string, pair<string, void(Scoreboard, string, string)>>("L.D", pair<string, void(Scoreboard, string, string)> ("INTEGER UNIT", &load)),
+			{"S.D", "INTEGER UNIT", &store},
+		}
+	);
+
+	myInput.InstructionUseStatus = {
+		{"L.D", {.clockCycles = {1, 1, 1, 1}}},
+		{"S.D", {.clockCycles = {1, 1, 1, 1}}},
+		{"ADD", {.clockCycles = {1, 1, 2, 1}}},
+		{"ADDI", {.clockCycles = {1, 1, 10, 1}, .units = 2}},
+		{"", {.clockCycles = {1, 1, 40, 1}}},
 	};
+
 	if(argc <= 1){
-		myInput.instructions.push_back(AInput());
+		myInput.instructions.push_back(AInput("L.D", "F2", "0($1)"));
+		myInput.instructions.push_back(AInput("MUL.D", "F2", "0($1)"));
+		// todo: continue adding instructions from the assignment here
 	} else {
-		myInput.status
+		myInput.instructions
 	}
 	// insert memory and instructions before executing
 	myInput.process();
