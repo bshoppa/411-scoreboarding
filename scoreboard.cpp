5c0a819b487032e70aa8d01c71208a4b400f3ec5
Bailey Shoppa <bshoppa1@umbc.edu>
2020-12-05 21:21:17 -0500

    started implementing assembly operations

--- scoreboard.cpp
+++ scoreboard.cpp
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <vector>
 #include <iterator>
+#include <exception>
 using namespace std;
 
 typedef int clockCycle;
@@ -37,22 +38,22 @@ public:
 	string name;
 };
 
-struct AInput {
+struct Instruction {
 public:
-	AInput() {
+	Instruction() {
 		for(int i = 0; i < 4; i++){
 			clockCycleTimes[i] = 0;
 		}
 	};
-	AInput(string t_InstructionName, string t_reg1 = "", string t_reg2 = "", string t_reg3 = "") : InstructionName(t_InstructionName), reg1(t_reg1), reg2(t_reg2), reg3(t_reg3){
+	Instruction(string t_InstructionName, string t_reg1 = "", string t_reg2 = "", string t_reg3 = "") : InstructionName(t_InstructionName), reg1(t_reg1), reg2(t_reg2), reg3(t_reg3){
 		for(int i = 0; i < 4; i++){
 			clockCycleTimes[i] = 0;
 		}
 	};
-	AInput(const char* t_InstructionName, const char* t_reg1 = nullptr, const char* t_reg2 = nullptr, const char* t_reg3 = nullptr) : AInput((string) t_InstructionName, (string) t_reg1, (string) t_reg2, (string) t_reg3) {
+	Instruction(const char* t_InstructionName, const char* t_reg1 = nullptr, const char* t_reg2 = nullptr, const char* t_reg3 = nullptr) : Instruction((string) t_InstructionName, (string) t_reg1, (string) t_reg2, (string) t_reg3) {
 
 	}
-	AInput(const AInput &in){
+	Instruction(const Instruction &in){
 		InstructionName = in.InstructionName;
 		reg1 = in.reg1;
 		reg2 = in.reg2;
@@ -64,7 +65,7 @@ public:
 		hasExecuted = in.hasExecuted;
 	};
 
-	AInput& operator=(const AInput &in){
+	Instruction& operator=(const Instruction &in){
 		InstructionName = in.InstructionName;
 		reg1 = in.reg1;
 		reg2 = in.reg2;
@@ -77,7 +78,7 @@ public:
 		return *this;
 	};
 
-	~AInput(){
+	~Instruction(){
 
 	};
 	string InstructionName = "";
@@ -89,18 +90,18 @@ public:
 	clockCycle clockCycleTimes[4] = {0};
 	bool hasExecuted = false;
 
-	clockCycle ReadDependency(AInput &in){
+	clockCycle ReadDependency(Instruction &in){
 		if(reg1 == in.reg1){
 
 		}
 		return 0;
 	};
 
-	clockCycle WritebackDependency(AInput &in){
+	clockCycle WritebackDependency(Instruction &in){
 		return clockCycleTimes[3];
 	};
 
-	clockCycle Issue(AInput &in){
+	clockCycle Issue(Instruction &in){
 		if(ReadDependency(in)){
 
 		}
@@ -116,6 +117,18 @@ int max(int a, int b){
 }
 
 class Data {
+public:
+	Data() {
+
+	}
+	Data(int v){
+		data.i = (int) v;
+		datatype = INT;
+	};
+	Data(float v){
+		data.f = (float) v;
+		datatype = FLOAT;
+	};
 	union {
 		int i;
 		float f;
@@ -131,13 +144,15 @@ void find_next(const string buf, string &out){
 };
 
 void find_next(const string buf, string &out, string &nextbuf){
-	cout << buf << endl;
 	size_t next_comma = buf.find_first_of(',');
 	size_t next_space = buf.find_first_of(' ') + 1;
 
 	out = buf.substr(next_space, next_comma - next_space);
-	nextbuf = buf.substr(next_comma + 2, string::npos);
-	cout << "Next is " << nextbuf << endl;
+	if(next_comma == string::npos){
+
+	} else {
+		nextbuf = buf.substr(next_comma + 2, string::npos);
+	}
 };
 
 class Scoreboard {
@@ -150,9 +165,10 @@ public:
 	};
 	map<string, Executer> InstructionUseStatus;
 	vector<Subprocessor> Processor;
-	vector<AInput> Instructions;
-	vector<Data> Memory = vector<Data>(19, Data());
-	vector<Data> Registers = vector<Data>(32, Data());
+	vector<Instruction> Instructions;
+	vector<Data> Memory = vector<Data>(19, Data(0));
+	vector<Data> FRegisters = vector<Data>(32, Data((float) 0.));
+	vector<Data> IRegisters = vector<Data>(32, Data((int) 0));
 	bool complete;
 
 	Scoreboard (vector<pair<string, Subprocessor>> subprocessors, vector<pair<string, pair<string, CPUOperation>>> instruction_definitions) {
@@ -169,6 +185,9 @@ public:
 					break;
 				}
 			}
+			if(InstructionUseStatus.count(it->first) == (size_t) NULL){
+				cout << "WARNING: INSTRUCTION " << it->first << " NOT IN LIST OF SUPPORTED INSTRUCTIONS" << endl;
+			}
 		}
 
 		delete[] arr;
@@ -178,7 +197,7 @@ public:
 	void load_instructions(string file){
 		fstream dataFile;
 		string buf;
-		AInput temp;
+		Instruction temp;
 		temp.hasExecuted = false;
 
 		dataFile.open(file);
@@ -190,13 +209,17 @@ public:
 			string reg2_and_buf; find_next(reg1_and_buf, temp.reg2, reg2_and_buf);
 			string reg3_and_buf; find_next(reg2_and_buf, temp.reg3);
 
-			printf("Name \"%s\" reg1 \"%s\" reg2 \"%s\" reg3 \"%s\"\n", temp.InstructionName.c_str(), temp.reg1.c_str(), temp.reg2.c_str(), temp.reg3.c_str());
+			//printf("Name \"%s\" reg1 \"%s\" reg2 \"%s\" reg3 \"%s\"\n", temp.InstructionName.c_str(), temp.reg1.c_str(), temp.reg2.c_str(), temp.reg3.c_str());
 
 			Instructions.push_back(temp);
 		}
-	}
+	};
 
-	void tryToExecute(AInput &instruction, vector<AInput>::iterator iterator){
+	void load_memory(vector<Data> mem){
+		Memory = mem;
+	};
+
+	void tryToExecute(Instruction &instruction, vector<Instruction>::iterator iterator){
 		clockCycle whenCanStartIssue = 1;
 		clockCycle whenCanWriteBack = 1;
 		for(int index = distance(Instructions.begin(), iterator) - 1; index >= 0; index--){
@@ -214,6 +237,10 @@ public:
 		if(InstructionUseStatus.count(instruction.InstructionName) != (size_t) NULL){
 			Subprocessor* processor = InstructionUseStatus.at(instruction.InstructionName).part_of_cpu;
 			instruction.clockCycleTimes[2] = instruction.clockCycleTimes[1] + processor->clockCycles[2];
+
+			Scoreboard thisScoreboard = *this;
+			(InstructionUseStatus.at(instruction.InstructionName).operation.operation)(thisScoreboard, instruction.reg1, instruction.reg2, instruction.reg3);
+			*this = thisScoreboard;
 		} else {
 			cout << "Not setting clockCycleTimes[2] until mapping is implemented" << endl;
 			instruction.clockCycleTimes[2] = instruction.clockCycleTimes[1] + 1;
@@ -240,7 +267,7 @@ public:
 		instruction.clockCycleTimes[3] = whenCanWriteBack;
 		//
 		instruction.hasExecuted = true;
-	}
+	};
 
 	void process() {
 
@@ -248,30 +275,85 @@ public:
 		// start at instruction[0];
 		complete = false;
 		int currentClockCycle = 0;
-		for(vector<AInput>::iterator CurrentInstruction = Instructions.begin(); CurrentInstruction != Instructions.end(); CurrentInstruction++){
+		for(vector<Instruction>::iterator CurrentInstruction = Instructions.begin(); CurrentInstruction != Instructions.end(); CurrentInstruction++){
 			tryToExecute(*CurrentInstruction, CurrentInstruction);
 		}
 	}
 
 };
 
-void load(Scoreboard &scoreboard, string arg1, string arg2, string arg3) {
+size_t parse_register(string arg){
+	return stoi(arg.substr(1));
+}
+size_t parse_address(string arg){
+	size_t start_of_address = arg.find_first_of('(');
+	size_t end_of_address = arg.find_first_of(')');
+	if(start_of_address != string::npos){
+		return stoi(arg.substr(start_of_address + 1, (end_of_address - start_of_address) - 1));
+	}
+	throw invalid_argument("unable to parse offset and address of load instruction");
+	//return 0;
+}
+size_t parse_offset(string arg){
+	size_t start_of_address = arg.find_first_of('(');
+	return stoi(arg.substr(0, start_of_address));
+}
 
+void load(Scoreboard &scoreboard, string arg1, string arg2, string arg3) {
+	size_t address_in_F = parse_register(arg1);
+	size_t address_in_mem = parse_address(arg2);
+	size_t offset_in_mem = parse_offset(arg2);
+
+	if(address_in_mem + offset_in_mem >= scoreboard.Memory.size()){
+		throw out_of_range("position in memory out of bounds");
+	}
+	if(address_in_mem + offset_in_mem < 0){
+		throw out_of_range("position in memory out of bounds");
+	}
+	if(address_in_F >= scoreboard.FRegisters.size()){
+		throw out_of_range("position in floating point registers out of bounds");
+	}
+	if(address_in_F < 0){
+		throw out_of_range("position in floating point registers out of bounds");
+	}
+
+	scoreboard.FRegisters[address_in_F] = Data((float) scoreboard.Memory[address_in_mem + offset_in_mem].data.i);
 };
 void store(Scoreboard &scoreboard, string arg1, string arg2, string arg3) {
+	size_t address_in_F = parse_register(arg1);
+	size_t address_in_mem = parse_address(arg2);
+	size_t offset_in_mem = parse_offset(arg2);
 
+	if(address_in_mem + offset_in_mem >= scoreboard.Memory.size()){
+		throw out_of_range("position in memory out of bounds");
+	}
+	if(address_in_mem + offset_in_mem < 0){
+		throw out_of_range("position in memory out of bounds");
+	}
+	if(address_in_F >= scoreboard.FRegisters.size()){
+		throw out_of_range("position in floating point registers out of bounds");
+	}
+	if(address_in_F < 0){
+		throw out_of_range("position in floating point registers out of bounds");
+	}
+
+	scoreboard.Memory[address_in_mem + offset_in_mem] = Data((int) scoreboard.FRegisters[address_in_F].data.f);
+};
+
+void int_add(Scoreboard &scoreboard, string arg1, string arg2, string arg3) {
+	cout << "adding i" << endl;
 };
 void fp_add(Scoreboard &scoreboard, string arg1, string arg2, string arg3) {
-
+	cout << "addbng" << endl;
 };
 void fp_subtract(Scoreboard &scoreboard, string arg1, string arg2, string arg3) {
-
+	cout << "subfdctrng" << endl;
 };
 void fp_multiply(Scoreboard &scoreboard, string arg1, string arg2, string arg3) {
-
+	cout << "mukltnfgping" << endl;
 };
 void fp_divide(Scoreboard &scoreboard, string arg1, string arg2, string arg3) {
-
+	cout << "diviseun" << endl;
 };
 
 int main(int argc, char* argv[]) {
@@ -285,7 +367,7 @@ int main(int argc, char* argv[]) {
 		{
 			pair<string, pair<string, CPUOperation>>("L.D", pair<string, CPUOperation> ("INTEGER UNIT", CPUOperation{&load})),
 			pair<string, pair<string, CPUOperation>>("S.D", pair<string, CPUOperation> ("INTEGER UNIT", CPUOperation{&store})),
-			pair<string, pair<string, CPUOperation>>("ADDI", pair<string, CPUOperation> ("INTEGER UNIT", CPUOperation{&fp_add})),
+			pair<string, pair<string, CPUOperation>>("ADDI", pair<string, CPUOperation> ("INTEGER UNIT", CPUOperation{&int_add})),
 			pair<string, pair<string, CPUOperation>>("MUL.D", pair<string, CPUOperation> ("FP MULTIPLIER", CPUOperation{&fp_multiply})),
 			pair<string, pair<string, CPUOperation>>("DIV.D", pair<string, CPUOperation> ("FP DIVIDER", CPUOperation{&fp_divide})),
 			pair<string, pair<string, CPUOperation>>("ADD.D", pair<string, CPUOperation> ("FP ADDER", CPUOperation{&fp_add})),
@@ -301,14 +383,40 @@ int main(int argc, char* argv[]) {
 		{"", {.clockCycles = {1, 1, 40, 1}}},
 	};*/
 
+	{
+		const vector<Data> data {
+			45,
+			12,
+			0,
+			0,
+			10,
+			135,
+			254,
+			127,
+			18,
+			4,
+			55,
+			8,
+			2,
+			98,
+			13,
+			5,
+			233,
+			158,
+			167,
+		};
+		myInput.load_memory(data);
+	}
+
+
 	if(argc <= 1){
-		myInput.Instructions.push_back(AInput ("L.D", "F2", "0($1)"));
-		myInput.Instructions.push_back(AInput ("MUL.D", "F4", "F2", "F0"));
-		myInput.Instructions.push_back(AInput ("L.D", "F6", "0($2)"));
-		myInput.Instructions.push_back(AInput ("ADD.D", "F6", "F4", "F2"));
-		myInput.Instructions.push_back(AInput ("S.D", "F6", "0", "F2"));
-		myInput.Instructions.push_back(AInput ("SUB.D", "F3", "F6", "F0"));
-		myInput.Instructions.push_back(AInput ("S.D", "F3", "0", "F3"));
+		myInput.Instructions.push_back(Instruction ("L.D", "F2", "0($1)"));
+		myInput.Instructions.push_back(Instruction ("MUL.D", "F4", "F2", "F0"));
+		myInput.Instructions.push_back(Instruction ("L.D", "F6", "0($2)"));
+		myInput.Instructions.push_back(Instruction ("ADD.D", "F6", "F4", "F2"));
+		myInput.Instructions.push_back(Instruction ("S.D", "F6", "0", "F2"));
+		myInput.Instructions.push_back(Instruction ("SUB.D", "F3", "F6", "F0"));
+		myInput.Instructions.push_back(Instruction ("S.D", "F3", "0", "F3"));
 	} else {
 		//myInput.Instructions
 		cout << "File is " << string(argv[1]) << endl;
@@ -320,6 +428,14 @@ int main(int argc, char* argv[]) {
 	for(size_t i = 0; i < myInput.Instructions.size(); i++){
 		auto Instruction = myInput.Instructions[i];
 		printf("%-7s%-6s%-6s%-6s%-10i%-10i%-10i%-10i\n", Instruction.InstructionName.c_str(), Instruction.reg1.c_str(), Instruction.reg2.c_str(), Instruction.reg3.c_str(), Instruction.clockCycleTimes[0], Instruction.clockCycleTimes[1], Instruction.clockCycleTimes[2], Instruction.clockCycleTimes[3]);
+	}
+	printf("F Registers:\n");
+	for(size_t i = 0; i < myInput.FRegisters.size(); i++){
+		printf("F%i: %f\n", i, myInput.FRegisters[i].data.f);
+	}
+	printf("I Registers:\n");
+	for(size_t i = 0; i < myInput.IRegisters.size(); i++){
+		printf("$%i: %i\n", i,  myInput.IRegisters[i].data.i);
 	}
 	return 0;
 }
